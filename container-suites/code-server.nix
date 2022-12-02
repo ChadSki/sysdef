a6873323131ce1f3d40597a126ddf4f9c2bfc416
Chad Zawistowski <chadz.ski@protonmail.com>
2022-12-02 18:09:19 -0500

    WIP code-server

--- container-suites/code-server.nix
+++ container-suites/code-server.nix
@@ -4,7 +4,8 @@
     services = {
       code-server = {
         enable = true;
-        hashedPassword = "$argon2i$v=19$m=4096,t=3,p=1$wst5qhbgk2lu1ih4dmuxvg$ls1alrvdiwtvzhwnzcm1dugg+5dto3dt1d5v9xtlws4";
+        auth = "none";
+        host = "code-server.mydomain.test";
       };
     };
   };
